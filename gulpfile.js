60d1dac097290e6b63a22fcbcdbf725b5519418d
Noel Cothren <noelcothren@gmail.com>
2017-06-13 20:58:08 -0600

    added gulpif and minify

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
     concat = require('gulp-concat'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
     connect = require('gulp-connect');
 
 var env,
@@ -48,6 +50,7 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
+    .pipe(gulpif(env==='production', uglify()))
     .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload())
 });
